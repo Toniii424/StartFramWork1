312c508556a579d456c6b8a2fd9c80b040002857
Toniii424 <osamaanton292@gmail.com>
2025-09-28 20:56:15 +0300

    Navbar button

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -7,9 +7,11 @@ export default function Navbar() {
 <nav className={`navbar navbar-expand-lg fixed-top py-4 ${style.nav}`}>
   <div className={`container ${style.styleNav}`}>
     <Link className="navbar-brand text-white text-uppercase fw-bolder fs-2" to="/">Start Framework </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
+<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span className="navbar-toggler-icon" />
+</button>
+
+
     <div className="collapse navbar-collapse d-flex" id="navbarNav">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item me-3">
